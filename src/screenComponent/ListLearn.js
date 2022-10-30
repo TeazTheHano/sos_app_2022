ba7028c9f8e611b18533f2d8b4d761ea09147db9
TeazTheHano <teaz.khuonganhkiet@gmail.com>
2022-10-30 15:21:42 +0700

    kak update

--- src/screenComponent/ListLearn.js
+++ src/screenComponent/ListLearn.js
@@ -43,8 +43,19 @@ const categories = [
 const ListLearn = () => {
   const [cateNum, setCateNum] = useState(null)
   const [showQuiz, setShowQuiz] = useState(false)
-  const [borderColorAns, setBorderColorAns] = useState('gray')
-
+  const [borderColorAns1, setBorderColorAns1] = useState(false)
+  const [borderColorAns2, setBorderColorAns2] = useState(false)
+  const [borderColorAns3, setBorderColorAns3] = useState(false)
+  const [optionBackground1, setOptionBackground1 ] = useState('white')
+  const [optionBackground2, setOptionBackground2 ] = useState('white')
+  const [optionBackground3, setOptionBackground3 ] = useState('white')
+  const check = () => {
+    if (datas[cateNum][0].ans) {
+      
+    } else {
+      
+    }
+  }
   const navigation = useNavigation();
 
   const listOfContent = () => {
@@ -54,10 +65,11 @@ const ListLearn = () => {
           {categories.map((categories) => (
             <TouchableOpacity
               key={categories.category}
-              onPress={() => { console.log(categories.category),
+              onPress={() => {
+                console.log(categories.category),
                 setCateNum(categories.category),
                 console.log('hehe' + cateNum)
-                // navigation.navigate()
+                setShowQuiz(true)
               }}
             >
               <View
@@ -94,15 +106,62 @@ const ListLearn = () => {
       <ScrollView showsVerticalScrollIndicator={false}>
       {/* QUEST 1 */}
       <View>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>{datas[1][0].ques}</Text>
-        <TouchableOpacity>
-          <View style={{
+        <Text style={{fontSize: 18, fontWeight: 'bold'}}>{datas[cateNum][0].ques}</Text>
+        <TouchableOpacity
+          key={datas[1][0].choose[0]}
+          onPress={() => {
+            setBorderColorAns1(!borderColorAns1)
+            console.log('1')
+          }}
+        >
+          <View 
+            borderColor = {borderColorAns1? 'orange' : '#76B9FF'}
+            style={{
             width: 333,
             borderWidth: 2,
             marginTop: 10,
-            borderColor: {borderColorAns}
+            borderRadius: 15,
+            backgroundColor: optionBackground1
           }}>
             <Text style={{fontSize: 18, paddingVertical: 20, paddingHorizontal: 2, alignSelf: 'center', width: 300}}>{datas[1][0].choose[0]}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          key={datas[1][0].choose[1]}
+          onPress={() => {
+            setBorderColorAns2(!borderColorAns2)
+            console.log('1')
+          }}
+        >
+          <View 
+            borderColor = {borderColorAns2? 'orange' : '#76B9FF'}
+            style={{
+            width: 333,
+            borderWidth: 2,
+            marginTop: 10,
+            borderRadius: 15,
+            backgroundColor: optionBackground2
+          }}>
+            <Text style={{fontSize: 18, paddingVertical: 20, paddingHorizontal: 2, alignSelf: 'center', width: 300}}>{datas[1][0].choose[1]}</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          key={datas[1][0].choose[2]}
+          onPress={() => {
+            setBorderColorAns3(!borderColorAns3)
+            console.log('1')
+          }}
+        >
+          <View 
+            borderColor = {borderColorAns3? 'orange' : '#76B9FF'}
+            style={{
+            width: 333,
+            borderWidth: 2,
+            marginTop: 10,
+            borderRadius: 15,
+            backgroundColor: optionBackground3
+          }}>
+            <Text style={{fontSize: 18, paddingVertical: 20, paddingHorizontal: 2, alignSelf: 'center', width: 300}}>{datas[1][0].choose[2]}</Text>
           </View>
         </TouchableOpacity>
       </View>
